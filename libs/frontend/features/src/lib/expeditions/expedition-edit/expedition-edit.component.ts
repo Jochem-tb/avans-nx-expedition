7a006d731c8d80fffb3405eccdece4640028d5ce
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2024-12-02 13:19:02 +0100

    Base edit componennt + enums

--- libs/frontend/features/src/lib/expeditions/expedition-edit/expedition-edit.component.ts
+++ libs/frontend/features/src/lib/expeditions/expedition-edit/expedition-edit.component.ts
@@ -1,30 +1,53 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ExpeditionService } from '../expedition.service';
+import { Expedition } from '@avans-nx-expedition/backend/expedition';
+import { Subscription } from 'rxjs';
 import {
-    MatDialogRef,
-    MAT_DIALOG_DATA,
-    MatDialog
-} from '@angular/material/dialog';
-import { IExpedition } from '@avans-nx-expedition/shared/api';
+    ContinentEnum,
+    DifficultyLevel,
+    ExpeditionStatus
+} from '@avans-nx-expedition/shared/api';
 
 @Component({
     selector: 'avans-nx-expedition-expedition-edit',
     templateUrl: './expedition-edit.component.html',
     styleUrls: ['./expedition-edit.component.css']
 })
-export class ExpeditionEditComponent {
+export class ExpeditionEditComponent implements OnInit {
+    expeditionId: string | null = null;
+    expedition: Expedition | undefined;
+    difficultyLevels = Object.values(DifficultyLevel);
+    statusus = Object.values(ExpeditionStatus);
+    continents = Object.values(ContinentEnum);
+    sub: Subscription = new Subscription();
+
     constructor(
-        public dialog: MatDialog,
-        public dialogRef: MatDialogRef<ExpeditionEditComponent>
+        private route: ActivatedRoute,
+        private expeditionService: ExpeditionService
     ) {}
 
-    public expedtion: IExpedition | undefined;
-
-    onSubmit(formValue: any): void {
-        console.log('Form Submitted:', formValue);
-        this.dialogRef.close(); // Close the dialog after submission
+    ngOnInit(): void {
+        this.route.paramMap.subscribe((params) => {
+            this.expeditionId = params.get('id');
+            this.expeditionService
+                .getExpeditionById(String(this.expeditionId))
+                .subscribe((expedition) => {
+                    this.expedition = expedition;
+                    console.log('Expedition:', this.expedition);
+                });
+        });
     }
 
-    onCancel(): void {
-        console.log('onCancel() called');
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
+
+    // removeParticipant(participantId: string): void {
+    //     this.expeditionService
+    //         .removeParticipant(this.expeditionId, participantId)
+    //         .subscribe((expedition) => {
+    //             this.expedition = expedition;
+    //         });
+    // }
 }
