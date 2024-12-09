44049b92cec05bcce78e64443efbeb31ffacd684
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2024-12-09 11:03:31 +0100

    Hotfixes after branch change

--- libs/frontend/account/src/lib/login/login.component.spec.ts
+++ libs/frontend/account/src/lib/login/login.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { UserListComponent } from './user-list.component';
+import { LoginComponent } from './login.component';
 
 describe('UserListComponent', () => {
-    let component: UserListComponent;
-    let fixture: ComponentFixture<UserListComponent>;
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [UserListComponent]
+            declarations: [LoginComponent]
         }).compileComponents();
 
-        fixture = TestBed.createComponent(UserListComponent);
+        fixture = TestBed.createComponent(LoginComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
