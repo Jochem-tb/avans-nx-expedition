4de1b8ad010c8db00d9a20a977da846205b6983e
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2025-04-15 11:46:48 +0200

    Remove share-a-meal-app

--- apps/share-a-meal-web/src/app/components/about/about.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'avans-nx-expedition-about',
-    templateUrl: './about.component.html',
-    styles: []
-})
-export class AboutComponent {}
